f1f48b64145d6f3e8479822728e194789d18e41c
Duan Hao <haoduan@Duans-MacBook-Pro.local>
2018-04-30 19:20:46 +1200

    ###changed - bug fix

--- utils.go
+++ utils.go
@@ -3,23 +3,31 @@ package utils
 import "strings"
 
 func MapKeyToCamelCase(m map[string]interface{}, d string) map[string]interface{} {
-  for k, v := range m {
-    nk := ""
-    s := strings.Split(k, d)
-    if len(s) > 1 {
-      for i, kp := range s {
-        kp = strings.ToLower(kp)
-        if i == 0 {
-          nk = nk + kp
-        } else {
-          nk = nk + strings.Title(kp)
-        }
-      }
-    } else {
-      nk = k
-    }
-    m[nk] = v
-    delete(m, k)
-  }
-  return m
+	for k, v := range m {
+		nk := ""
+		s := strings.Split(k, d)
+		if len(s) > 1 {
+			var r []string
+			for _, v := range s {
+				if v != "" {
+					r = append(r, v)
+				}
+			}
+			for i, kp := range s {
+				kp = strings.ToLower(kp)
+				if i == 0 {
+					nk = nk + kp
+				} else {
+					nk = nk + strings.Title(kp)
+				}
+			}
+		} else {
+			nk = k
+		}
+		m[nk] = v
+		if len(s) > 1 {
+			delete(m, k)
+		}
+	}
+	return m
 }
