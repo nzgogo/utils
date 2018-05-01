b285bb3b7fdcf1c97e1a82b1e4ea62d07d9d072a
chadhao <haoduan@outlook.com>
2018-05-01 22:42:01 +1200

    ###changed - Contains() to ContainsString()

--- utils.go
+++ utils.go
@@ -1,10 +1,6 @@
 package utils
 
 import (
-	"bytes"
-	"crypto/md5"
-	"encoding/gob"
-	"encoding/hex"
 	"strings"
 )
 
@@ -38,34 +34,12 @@ func MapKeyToCamelCase(m map[string]interface{}, d string) map[string]interface{
 	return m
 }
 
-func Contains(s interface{}, v interface{}) bool {
-	sSlice := s.([]interface{})
-	vBytes, err := getBytes(v)
-	if err != nil {
-		return false
-	}
-	vMd5Bytes := md5.Sum(vBytes)
-	vString := hex.EncodeToString(vMd5Bytes[:])
-	set := make(map[string]struct{}, len(sSlice))
-	for _, e := range sSlice {
-		eBytes, err := getBytes(e)
-		if err != nil {
-			continue
-		}
-		eMd5Bytes := md5.Sum(eBytes)
-		eString := hex.EncodeToString(eMd5Bytes[:])
-		set[eString] = struct{}{}
+func ContainsString(s []string, v string) bool {
+	set := make(map[string]struct{}, len(s))
+	for _, e := range s {
+		set[e] = struct{}{}
 	}
 
-	_, ok := set[vString]
+	_, ok := set[v]
 	return ok
-}
-
-func getBytes(value interface{}) ([]byte, error) {
-	var buf bytes.Buffer
-	enc := gob.NewEncoder(&buf)
-	if err := enc.Encode(value); err != nil {
-		return nil, err
-	}
-	return buf.Bytes(), nil
 }
