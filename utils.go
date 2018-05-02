490f36d2b5a1eb0d9f3c320f4bb6b69a830b98be
chadhao <haoduan@outlook.com>
2018-05-02 17:28:25 +1200

    ###added - MapKeySwap()

--- utils.go
+++ utils.go
@@ -1,37 +1,43 @@
 package utils
 
-import (
-	"strings"
-)
+// func MapKeyToCamelCase(m map[string]interface{}, d string) map[string]interface{} {
+// 	for k, v := range m {
+// 		nk := ""
+// 		s := strings.Split(k, d)
+// 		if len(s) > 1 {
+// 			var r []string
+// 			for _, v := range s {
+// 				if v != "" {
+// 					r = append(r, v)
+// 				}
+// 			}
+// 			for i, kp := range s {
+// 				kp = strings.ToLower(kp)
+// 				if i == 0 {
+// 					nk = nk + kp
+// 				} else {
+// 					nk = nk + strings.Title(kp)
+// 				}
+// 			}
+// 		} else {
+// 			nk = k
+// 		}
+// 		m[nk] = v
+// 		if len(s) > 1 {
+// 			delete(m, k)
+// 		}
+// 	}
+// 	return m
+// }
 
-func MapKeyToCamelCase(m map[string]interface{}, d string) map[string]interface{} {
+func MapKeySwap(m map[string]interface{}, n map[string]string) map[string]interface{} {
+	r := make(map[string]interface{})
 	for k, v := range m {
-		nk := ""
-		s := strings.Split(k, d)
-		if len(s) > 1 {
-			var r []string
-			for _, v := range s {
-				if v != "" {
-					r = append(r, v)
-				}
-			}
-			for i, kp := range s {
-				kp = strings.ToLower(kp)
-				if i == 0 {
-					nk = nk + kp
-				} else {
-					nk = nk + strings.Title(kp)
-				}
-			}
-		} else {
-			nk = k
-		}
-		m[nk] = v
-		if len(s) > 1 {
-			delete(m, k)
+		if n[k] != "" {
+			r[n[k]] = v
 		}
 	}
-	return m
+	return r
 }
 
 func ContainsString(s []string, v string) bool {
