6cb4ae5403f0d028c00745a8e858c4a410e623d6
chadhao <haoduan@outlook.com>
2018-05-06 19:08:31 +1200

    ###added - ShortID()

--- utils.go
+++ utils.go
@@ -1,5 +1,10 @@
 package utils
 
+import (
+	"fmt"
+	"strconv"
+)
+
 // func MapKeyToCamelCase(m map[string]interface{}, d string) map[string]interface{} {
 // 	for k, v := range m {
 // 		nk := ""
@@ -48,4 +53,45 @@ func ContainsString(s []string, v string) bool {
 
 	_, ok := set[v]
 	return ok
+}
+
+func ShortID(s string) (string, error) {
+	if _, err := strconv.ParseInt(s, 16, 64); err != nil {
+		return "", err
+	}
+
+	inputSlice := []rune(s)
+
+	var inputInt []int64
+
+	for i := 0; i < len(s); i = i + 4 {
+		fmt.Println(string(inputSlice[i : i+4]))
+		n, _ := strconv.ParseInt(string(inputSlice[i:i+4]), 16, 64)
+		inputInt = append(inputInt, n)
+	}
+
+	for i, v := range inputInt {
+		inputInt[i] = remainder(v)
+	}
+
+	table := "23456789ABCDEFGHJKLMNPQRSTUVWXYZ"
+
+	var result string
+	for i := len(inputInt) - 1; i >= 0; i-- {
+		result = result + fmt.Sprintf("%c", table[inputInt[i]])
+	}
+
+	return result, nil
+}
+
+func remainder(n int64) int64 {
+	if n <= 32 {
+		return n
+	}
+	var totalRemainder int64
+	for n > 0 {
+		totalRemainder = totalRemainder + n%32
+		n = n / 32
+	}
+	return remainder(totalRemainder)
 }
